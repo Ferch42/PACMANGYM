76934601c2e5df9fa6466701b2d529f1578591ff
Ferch42 <fernando.hsieh@usp.br>
2025-01-22 08:08:09 -0300

    df

--- markov_chains.py
+++ markov_chains.py
@@ -27,6 +27,8 @@ ACTION_DICT = {
 }
 
 GOALS = ['🍜' ,'🥗' ,'🍸' ,'🍹' ,'🌮' ]
+TRAJECTORY = []
+
 
 q = dict()
 
@@ -39,7 +41,10 @@ def Q(state):
 
 def reset():
 
+    global TRAJECTORY
+
     POINTS['🤖'] = (25,25)
+    TRAJECTORY = [POINTS['🤖']]
 
 
 def render():
@@ -68,7 +73,7 @@ def render():
 
 def step(action, goal):
 
-    global POINTS, STOP_PROB, ACTION_DICT, N
+    global POINTS, STOP_PROB, ACTION_DICT, TRAJECTORY,N
 
     X, Y = POINTS['🤖']
     
@@ -79,6 +84,8 @@ def step(action, goal):
         Y = min(max(Y+D_Y, 0), N-1)
 
     POINTS['🤖'] = (X,Y)
+
+    TRAJECTORY.append(POINTS['🤖'])
     
     reward = int(POINTS['🤖'] == POINTS[goal])
     
@@ -90,18 +97,21 @@ def run_policy(goal, t):
     
     ss = POINTS['🤖']
     for i in range(t):
-
+        
         ss , reward = step(Q((POINTS['🤖'], goal)).argmax(), goal)
 
         if reward==1:
-            return ss,t-(i+1), True
+            return ss,(i+1), True
     
     return ss, t, False
 
 
 def main():
 
+    global TRAJECTORY
+    
     goal = '🥗'
+    reset()
     for i in tqdm(range(1_000_000)):
 
         s = POINTS['🤖']
@@ -135,9 +145,66 @@ def main():
 
     print(ep_len)
 
-    counts, bins = np.histogram(ep_len, bins=200)
-    plt.stairs(counts, bins, fill=True)
-    plt.show()
+    #counts, bins = np.histogram(ep_len, bins=200)
+    #plt.stairs(counts, bins, fill=True)
+    #plt.show()
+
+
+    reset()
+
+    T_MAX= 200
+    t = 0
+    high_level_activations = []
+    
+    #plan = ['🥗', '🍸', '🌮']
+    plan = ['🥗'] 
+    
+    for p in plan:
+        
+        high_level_activations.append((p, t))
+        
+        ss, time_spent, done = run_policy(p, T_MAX - t)
+        t += time_spent
+        
+    print("++++++++++++++++++++++++++++++++++")
+    print(TRAJECTORY)
+    print(high_level_activations)
+
+    SUCCESS_DICT = {}
+    TRIALS_DICT = {}
+
+    for episode in range(100_000):
+        
+        reset()
+        run_policy('🥗', 25)
+
+        for i in range(len(TRAJECTORY)-1):
+            key = (TRAJECTORY[i], '🥗', len(TRAJECTORY)- i - 1)
+            #print(key)
+            if key not in TRIALS_DICT:
+                TRIALS_DICT[key] = 0
+                SUCCESS_DICT[key] = 0
+            
+            SUCCESS_DICT[key] +=1
+            TRIALS_DICT[key] +=1
+            
+        for j in range(len(TRAJECTORY) -1):
+
+            for k in range(len(TRAJECTORY) -2 - j):
+                #print(k, len(TRAJECTORY)- j - 2)
+
+                key = (TRAJECTORY[k], '🥗', len(TRAJECTORY)- j - 1)
+
+                if key not in TRIALS_DICT:
+                    TRIALS_DICT[key] = 0
+                    SUCCESS_DICT[key] = 0
+
+                TRIALS_DICT[key] +=1
+
+
+    PROB_DICT = {k: SUCCESS_DICT[k]/(TRIALS_DICT[k]) for k in TRIALS_DICT.keys()}
+    print(PROB_DICT)
+
 
 
 
